19f69b5fd0841bcbade71282f44c9217d5739dde
Nathashasathsarani <160476994+Nathashasathsarani@users.noreply.github.com>
2024-10-28 12:15:12 +0530

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,1 +1,26 @@
+// src/components/header/header.tsx
+import Link from 'next/link';
 
+export default function Header() {
+  return (
+    <header className="bg-gray-800 shadow-md py-4">
+      <nav className="flex justify-center space-x-10">
+        <Link href="/" className="text-lg font-semibold text-gray-300 hover:text-blue-400 transition-colors duration-200">
+          Home
+        </Link>
+        <Link href="/about" className="text-lg font-semibold text-gray-300 hover:text-blue-400 transition-colors duration-200">
+          About
+        </Link>
+        <Link href="/skills" className="text-lg font-semibold text-gray-300 hover:text-blue-400 transition-colors duration-200">
+          Skills
+        </Link>
+        <Link href="/projects" className="text-lg font-semibold text-gray-300 hover:text-blue-400 transition-colors duration-200">
+          Projects
+        </Link>
+        <Link href="/contact" className="text-lg font-semibold text-gray-300 hover:text-blue-400 transition-colors duration-200">
+          Contact
+        </Link>
+      </nav>
+    </header>
+  );
+}
